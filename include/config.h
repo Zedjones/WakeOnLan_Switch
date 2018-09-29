2f84995226a55868171736ba219bb8347c17b888
Nicholas <dojoman19@gmail.com>
2018-09-29 19:29:40 -0400

    Started working on config header

---
+++ include/config.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h> 
+#include <string.h>
+
+#ifndef _CONFIG_HEADER_
+#define _CONFIG_HEADER_
+
+#define CONFIG_FILE "./configurations"
+
+typedef struct WoLConfig_t {
+    char* broadcast_address;
+    char* mac_address;
+    int udp_port; 
+} WolConfig; 
+
+WolConfig** get_configs();
+
+WolConfig* create_config(char* broadcast, char* mac, int port);
+
+void destroy_config(WolConfig* config);
+
+#endif
